c1b3f87055b15e1bc09c3ee6dc06f853fbdf4b65
firstforest <h.morikosi@gmail.com>
2022-02-23 23:33:53 +0900

    バーで反射できるようにします

--- src/Game.hs
+++ src/Game.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
 
 module Game where
@@ -18,10 +19,14 @@ ballSpeed = 500
 
 initialize :: System' ()
 initialize = do
-  newEntity (Position $ V2 (Const.width / 2) (Const.height - 20), Bar 100)
+  newEntity
+    ( Position $ V2 (Const.width / 2) (Const.height - 20),
+      Bar 100,
+      Collider 0 0 100 10
+    )
   newEntity
     ( Position $ V2 (Const.width / 2) (Const.height - 30),
-      Ball 10,
+      Ball 5,
       Velocity (ballSpeed *^ normalize (V2 10 (-3)))
     )
   newEntity (Collider (-50) 0 0 Const.height) --left
@@ -56,8 +61,17 @@ checkCollision left top right bottom x y radius
 step :: Float -> System' ()
 step dt = do
   cmap $ \(Position p, Velocity v) -> Position ((dt *^ v) + p)
-  cmapM_ $ \(Collider left top right bottom) -> do
+  cmapM_ $ \(Collider left top right bottom, _ :: Not Position) -> do
     let check = checkCollision left top right bottom
+    cmap $ \(Ball radius, Position (V2 x y), v@(Velocity (V2 vx vy))) ->
+      case check x y radius of
+        ToLeft -> Velocity (V2 (- (abs vx)) vy)
+        ToRight -> Velocity $ V2 (abs vx) vy
+        ToUp -> Velocity $ V2 vx (- (abs vy))
+        ToDown -> Velocity $ V2 vx (abs vy)
+        AsIs -> v
+  cmapM_ $ \(Collider left top right bottom, Position (V2 x y)) -> do
+    let check = checkCollision (left - right / 2 + x) (top - bottom / 2 + y) (right - right / 2 + x) (bottom - bottom / 2 + y)
     cmap $ \(Ball radius, Position (V2 x y), v@(Velocity (V2 vx vy))) ->
       case check x y radius of
         ToLeft -> Velocity (V2 (- (abs vx)) vy)
